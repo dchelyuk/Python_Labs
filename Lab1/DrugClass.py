c79687d5dbc79afcec682432b763255c656e1720
Danylo Chelyuk <dchelyuk@gmail.com>
2021-02-28 21:48:51 +0200

    Modifying DrugClass.py: creating class Drug and main() function

--- Lab1/DrugClass.py
+++ Lab1/DrugClass.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+class Drug:
+    drug_type = "pill"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, name, volume_of_active_substance, weight_in_mg, active_substance_name, max_doses_per_day):
+        self.name = name
+        self.volume_of_active_substance = volume_of_active_substance
+        self.weight_in_mg = weight_in_mg
+        self.active_substance_name = active_substance_name
+        self.max_doses_per_day = max_doses_per_day
+
+    def __del__(self):
+        return
+
+    def __str__(self):
+        return f"\nName: {self.name}\n" \
+               f"Volume of active substance: {self.volume_of_active_substance}\n" \
+               f"Weight in mg: {self.weight_in_mg}\n" \
+               f"Active substance name: {self.active_substance_name}\n" \
+               f"Maximum allowed doses per day: {self.max_doses_per_day}\n"
+
+    @staticmethod
+    def get_drug_type():
+        return Drug.drug_type
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    drug1 = Drug("Ibuprofen", 100, 300, "Ibuprom", 3)
+    drug2 = Drug("Nurofen", 20, 200, "Nurom", 20)
+    drug3 = Drug("Validol", 50, 400, "Validol", 10)
+
+    print(drug1, drug2, drug3)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Danylo')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
